aaca5fd5d89492f87b8643f113a7b566c2f3db2a
Xurshid Abdujalilov <abdxurshid01@mail.ru>
2023-05-26 17:50:30 +0500

    ...

---
+++ ClassHub/Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities.Assignments;
+using Domain.Entities.Groups;
+using Domain.Entities.Marks;
+using Domain.Entities.Notices;
+using Domain.Entities.Students;
+using Domain.Entities.Teachers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions
+{
+    public interface IApplicationDbContext:DbContext
+    {
+        public DbSet<Mark> Marks { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Notice> Notices{ get; set; }
+        public DbSet<Student> Students{ get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ ClassHub/Application/StartupApplication.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class StartupApplication
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        { 
+
+            // ... 
+            return services;
+        }
+    }
+}
